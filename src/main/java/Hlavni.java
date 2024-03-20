44421a885eb25d2f5baeb9478479158d616b300c
kseniyababaryka <babarks1@uhk.cz>
2024-03-20 13:39:51 +0100

    Rozsireni tridy Hlavni.

--- src/main/java/Hlavni.java
+++ src/main/java/Hlavni.java
@@ -1,10 +1,12 @@
 /*
- *Hlavni trida projektu
+ *Hlavni trida velkeho projektu
  * */
 
 public class Hlavni {
 
     public static void main(String[] args) {
         System.out.println("Main class");
+        System.out.println("Pridano v klonu");
     }
+
 }
